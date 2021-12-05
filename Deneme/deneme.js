f1207599828f48915f103fd3b62915545122bc03
Furkan Mamuş <furkanmamus1903@hotmail.com>
2021-12-05 15:40:13 +0300

    Delete Deneme directory

--- Deneme/deneme.js
+++
@@ -1,7 +0,0 @@
-let info = document.querySelector("#info")
-info.addEventListener("click", domClick)
-
-function domClick(){
-    console.log("JAVASCRİPT AWESOME")
-    this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
-}
