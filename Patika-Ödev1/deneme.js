9354ebcc052dc7fc8fa97982feb6780c297f9de5
mamus <furkanmamus1903@hotmail.com>
2021-12-05 15:38:20 +0300

    refactor date time

--- Patika-Ödev1/deneme.js
+++ Patika-Ödev1/deneme.js
@@ -2,6 +2,15 @@ let username = prompt("Adınızı Giriniz: ")
 let info = document.querySelector("#info")
 info.innerHTML = `Merhaba, ${username}! Hoşgeldin!`
 
-let d = new Date()
-const date = document.querySelector("#date")
-date.innerHTML = `${d}`
+setInterval(() => {
+    var date = new Date();
+    var dateStr =
+  ("00" + (date.getMonth() + 1)).slice(-2) + "/" +
+  ("00" + date.getDate()).slice(-2) + "/" +
+  date.getFullYear() + " " +
+  ("00" + date.getHours()).slice(-2) + ":" +
+  ("00" + date.getMinutes()).slice(-2) + ":" +
+  ("00" + date.getSeconds()).slice(-2);
+  const date2 = document.querySelector("#date")
+  date2.innerHTML = `${dateStr}`
+}, 100);
